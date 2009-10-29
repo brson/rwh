de9cc28cd3951a68e77e8ea5f7ac8a052233e2f4
brian <andersrb@gmail.com>
2009-10-29 18:53:14 -0400

    Create the JValue algebraic data type

---
+++ SimpleJSON/SimpleJSON.hs
@@ -0,0 +1,9 @@
+
+-- | An algebraic data type to represent the range of possible JSON types.
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+              deriving (Eq, Ord, Show)
