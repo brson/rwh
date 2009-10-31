8f838b2769988b242925fb6fee0fd033b5f6ad13
brian <andersrb@gmail.com>
2009-10-31 19:51:06 -0400

    Add renderJValue implementation for JBool

--- SimpleJSON/PutJSON.hs
+++ SimpleJSON/PutJSON.hs
@@ -5,4 +5,6 @@ import SimpleJSON
 renderJValue :: JValue -> String
 renderJValue (JString s) = show s
 renderJValue (JNumber n) = show n
+renderJValue (JBool True) = "true"
+renderJValue (JBool False) = "false"
 renderJValue _ = undefined
--- SimpleJSON/Test/PutJSONTests.hs
+++ SimpleJSON/Test/PutJSONTests.hs
@@ -2,6 +2,7 @@ module SimpleJSON.Test.PutJSONTests (tests) where
 
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.QuickCheck (testProperty)
+import Test.QuickCheck
 import SimpleJSON
 import SimpleJSON.PutJSON
 import SimpleJSON.Test.Utils ()
@@ -18,6 +19,18 @@ renderJValueTests =
 
     $ let prop :: Double -> Bool
           prop n = renderJValue (JNumber n) == show n
+      in prop
+
+    , testProperty "renderJValue should render a JBool containing True as 'true'"
+
+    $ let prop :: Bool -> Property
+          prop b = b ==> renderJValue (JBool b) == "true"
+      in prop
+
+    , testProperty "renderJValue should render a JBool containing False as 'false'"
+
+    $ let prop :: Bool -> Property
+          prop b = not b ==> renderJValue (JBool b) == "false"
       in prop
 
     ]
